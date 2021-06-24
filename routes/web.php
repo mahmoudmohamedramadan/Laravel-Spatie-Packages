5989370b7a276db9f5ff99ff373bcc329b544a81
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2021-06-24 10:07:39 +0200

    add `roles` and `permissions` routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,19 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserRolesController;
+use App\Http\Controllers\UserPermissionsController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Auth::routes();
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['prefix' => 'users'], function () {
+    Route::resource('/roles', UserRolesController::class)->except('show');
+    Route::resource('/roles.permissions', UserPermissionsController::class)->except('show');
 });
