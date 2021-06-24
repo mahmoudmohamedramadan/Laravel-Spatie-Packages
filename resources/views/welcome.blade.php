81ad04013eeac77147564fb8fb832b773f84bc40
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2021-06-24 10:05:21 +0200

    change `welcome.blade.php` title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ $view_name }} | {{ env("APP_NAME", "Laravel-Spatie-Packages") }}</title>
+        <title>Welcome | {{ env("APP_NAME", "Laravel-Spatie-Packages") }}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
