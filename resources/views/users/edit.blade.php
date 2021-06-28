17141fc3ccb4b58adbc9b3d68c68ba2f5f934ffc
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2021-06-28 13:07:05 +0200

    add `edit.blade.php` users view

---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('title', 'Edit User')
+
+@section('content')
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Edit User</div>
+                    <div class="card-body">
+                        <form action="{{ route('users.update', $user->id) }}" method="POST">
+                            @csrf
+                            @method('patch')
+
+                            <div class="form-group">
+                                <label>Name</label>
+                                <input type="text" class="form-control" name="name"
+                                    value="{{ $user->name ?? old('name') }}" autocomplete="off">
+                                @error('name')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <label>Email</label>
+                                <input type="email" class="form-control" name="email"
+                                    value="{{ $user->email ?? old('email') }}" autocomplete="off">
+                                @error('email')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
