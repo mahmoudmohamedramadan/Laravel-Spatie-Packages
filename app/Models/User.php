5fa0cb2a54fb5b607a7b31c20b9cf2f8332f29bc
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2021-06-28 13:05:26 +0200

    add and implement the `LogsActivity` trait

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,11 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, HasRoles;
+    use HasFactory, Notifiable, HasRoles, LogsActivity;
 
     /**
      * The attributes that are mass assignable.
@@ -41,4 +43,15 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            /* use the `logFillabel()` to store the all fillable attributes on the model */
+            // ->logFillable()
+            /* may be you've used the `guarded` property instead of the `fillable` property, and want to log the attributes that NOT in `guarded` (fillable) property, So you can use the `logUnguarded()` method */
+            // ->logUnguarded()
+            /* here I want to log the `name` and the `email` if any one of them has changed */
+            ->logOnly(['name', 'email']);
+    }
 }
