53ee333fe8ab121f032d546af69eb8c3df7fffdc
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2021-07-01 22:20:15 +0200

    add `tags` to the fillable property

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,15 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Tags\HasTags;
 
 class Post extends Model
 {
-    use HasFactory;
+    /* > NOTE that you have NOT add `tags` column to `posts` table  */
+    use HasFactory, HasTags;
 
     protected $fillable = [
         'user_id',
         'title',
         'body',
+        'tags',
     ];
 
     public function user()
