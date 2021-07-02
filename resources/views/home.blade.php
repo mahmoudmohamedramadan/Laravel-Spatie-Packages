652c58e3b5d542e219178058117b4bcd4bb42f58
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2021-07-02 22:04:30 +0200

    add `Feeds` in navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -31,8 +31,12 @@
                                         <a class="nav-link" href="{{ route('logs_activity.index') }}" target="_blank">Logs Activity</a>
                                     </li>
 
-                                    <li class="nav-item">
+                                    <li class="nav-item mr-2">
                                         <a class="nav-link" href="{{ route('media_library.create') }}" target="_blank">Media Library</a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('feeds.create') }}" target="_blank">Feeds</a>
                                     </li>
                                 </ul>
                             </div>
