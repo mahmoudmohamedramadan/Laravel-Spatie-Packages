fb82c2d0dc3caa7f19b909ef5ce6cb3b29832148
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2021-07-02 18:34:35 +0200

    customize `create.blade.php` permission view

--- resources/views/permissions/create.blade.php
+++ resources/views/permissions/create.blade.php
@@ -1,36 +1,30 @@
 @extends('layouts.app')
 
-@if (request()->routeIs('roles.permissions.create'))
-    @section('title', 'Create Permission')
-    @else
-    @section('title', 'Edit Permission')
-    @endif
+@section('title', request()->routeIs('roles.permissions.create') ? 'Create Permission' : 'Edit Permission')
 
-    @section('content')
+@section('content')
 
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                    <div class="card">
-                        <div class="card-header">
-                            @if (request()->routeIs('roles.create'))
-                                Create Permission
-                            @else
-                                Edit Permission
-                            @endif
-                        </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        @if (request()->routeIs('roles.create'))
+                            Create Permission
+                        @else
+                            Edit Permission
+                        @endif
+                    </div>
 
-                        <div class="card-body">
-                            @include('includes.success', ['key' => 'success'])
+                    <div class="card-body">
+                        @include('includes.success', ['key' => 'success'])
 
-                            @if (request()->routeIs('roles.permissions.create'))
-                                <form action="{{ route('roles.permissions.store', $role->id) }}" method="POST">
-                                @else
-                                    <form
-                                        action="{{ route('roles.permissions.update', ['role' => $role->id, 'permission' => $permission->id]) }}"
-                                        method="POST">
-                                        @method('patch')
-                                        <input type="hidden" name="id" value="{{ $permission->id }}">
+                        <form
+                            action="{{ request()->routeIs('roles.permissions.create') ? route('roles.permissions.store', $role->id) : route('roles.permissions.update', ['role' => $role->id, 'permission' => $permission->id]) }}"
+                            method="POST">
+                            @if (request()->routeIs('roles.permissions.edit'))
+                                @method('patch')
+                                <input type="hidden" name="id" value="{{ $permission->id }}">
                             @endif
                             @csrf
 
@@ -51,41 +45,41 @@
 
                             <a href="{{ route('roles.permissions.index', $role->id) }}">Show All
                                 Permissions</a>
-                            </form>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 
-        @if (request()->routeIs('roles.permissions.edit'))
-            <div class="modal fade" id="permissions_modal" data-backdrop="static" data-keyboard="false" tabindex="-1"
-                aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="staticBackdropLabel">Delete Confirmation</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            <h5>Are you sure from deleting this permission?</h5>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <form
-                                action="{{ route('roles.permissions.destroy', ['role' => $role->id, 'permission' => $permission->id]) }}"
-                                method="POST">
-                                @csrf
-                                @method('delete')
+    @if (request()->routeIs('roles.permissions.edit'))
+        <div class="modal fade" id="permissions_modal" data-backdrop="static" data-keyboard="false" tabindex="-1"
+            aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="staticBackdropLabel">Delete Confirmation</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <h5>Are you sure from deleting this permission?</h5>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <form
+                            action="{{ route('roles.permissions.destroy', ['role' => $role->id, 'permission' => $permission->id]) }}"
+                            method="POST">
+                            @csrf
+                            @method('delete')
 
-                                <button type="submit" class="btn btn-danger">Confirm</button>
-                            </form>
-                        </div>
+                            <button type="submit" class="btn btn-danger">Confirm</button>
+                        </form>
                     </div>
                 </div>
             </div>
-        @endif
+        </div>
+    @endif
 
-    @endsection
+@endsection
