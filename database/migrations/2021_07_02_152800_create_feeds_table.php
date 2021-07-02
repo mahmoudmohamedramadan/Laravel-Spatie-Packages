736ecedb493d1faf4f3e7fa5459f915a410051fc
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2021-07-02 22:05:04 +0200

    add `2021_07_02_152800_create_feeds_table.php`

---
+++ database/migrations/2021_07_02_152800_create_feeds_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFeedsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('feeds', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('summary');
+            $table->string('link');
+            $table->string('author_name');
+            $table->string('author_email');
+            $table->string('created_at');
+            $table->string('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('feeds');
+    }
+}
