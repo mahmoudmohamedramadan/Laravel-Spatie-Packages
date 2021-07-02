cf756d039e2e2f3078ec4af908525ba9a1bb8333
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2021-07-02 17:43:23 +0200

    add `margin-left` to the `label`

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -46,7 +46,7 @@
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                                    <label class="form-check-label" for="remember">
+                                    <label class="form-check-label" for="remember" style="margin-left: 18px">
                                         {{ __('Remember Me') }}
                                     </label>
                                 </div>
