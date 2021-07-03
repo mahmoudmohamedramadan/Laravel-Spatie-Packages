a8d8a518ea1c63a3da418c5895cebc3213d8e52c
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2021-07-03 21:26:15 +0200

    implements the `Searchable` interface

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Searchable\Searchable;
+use Spatie\Searchable\SearchResult;
 use Spatie\Tags\HasTags;
 
-class Post extends Model
+class Post extends Model implements Searchable
 {
     /* > NOTE that you have NOT add `tags` column to `posts` table  */
     use HasFactory, HasTags;
@@ -21,5 +23,16 @@ class Post extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function getSearchResult(): SearchResult
+    {
+        $url = route('posts.edit', $this->id);
+
+        return new \Spatie\Searchable\SearchResult(
+            $this,
+            $this->title,
+            $url,
+        );
     }
 }
